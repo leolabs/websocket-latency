d5fc1be3773669ccec5fff3f5b0cc0f64a546c86
Leo Bernard <admin@leolabs.org>
2021-06-28 20:22:52 +0200

    :tada: Initial commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import http from 'http';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {pingInterval: 1000});
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
+
+io.on("connection", (socket) => {
+  socket.on("time", (time: number, cb) => {
+    const diff = Date.now() - time;
+    console.log("Ping", diff);
+    cb(diff);
+  })
+});
+
+server.listen(3005, () => {
+  console.log('listening on *:3005');
+});
